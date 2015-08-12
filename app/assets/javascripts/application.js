2664d6393a0740f6aa9a86fc0c71587f5a98393e
Robin Wu <robwu@ucdavis.edu>
2015-08-11 22:01:09 -0700

    Working on navigation bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,16 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
+//= require underscore
+//= require backbone
+//= require pley
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
+
+
+// !!!!!!!!add serializeJSON!!!!!
---
+++ app/assets/javascripts/pley.js
@@ -0,0 +1,12 @@
+window.Pley = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+  }
+};
+
+$(document).ready(function(){
+  Pley.initialize();
+});
