99ed4ea168890c485b5624697709085ea06bd57f
Robin Wu <robwu@ucdavis.edu>
2015-08-12 14:12:54 -0700

    More Bootstrap added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root to: "static_pages#root"
 
-  resources :users, except: [:index]
+  resources :users, except: [:index, :show]
   resource :session, only: [:new, :create, :destroy]
 
   namespace :api, defaults: {format: :json} do
     resources :businesses
     resources :reviews, only: [:create, :update, :destroy]
+    resources :users, only: [:show]
   end
 end
