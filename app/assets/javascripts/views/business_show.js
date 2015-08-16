a0d7e21796c0a5460a4b178d2d509e2a939c3353
Robin Wu <robwu@ucdavis.edu>
2015-08-16 14:45:55 -0700

    Photos can be uploaded, implementation still needs to be improved

--- app/assets/javascripts/views/business_show.js
+++ app/assets/javascripts/views/business_show.js
@@ -12,6 +12,21 @@ Pley.Views.BusinessShow = Backbone.CompositeView.extend({
     // debugger;
     this.$el.html(this.template({business: this.model}));
     this.attachSubviews();
+    this.attachUploadWidget();
     return this;
+  },
+
+  attachUploadWidget: function() {
+    $("#cloudinary_widget").cloudinary_upload_widget(CLOUDINARY_OPTIONS,
+      function(error, result) {
+        var photo = new Pley.Models.Photo({business_id: this.model.id,
+          path: result[0].url
+        });
+        photo.save({}, {
+          success: function() {
+            this.model.photos().add(photo);
+          }.bind(this)
+        });
+    }.bind(this));
   }
 });
